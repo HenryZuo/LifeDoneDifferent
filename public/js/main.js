faa6a5a8bf541fad688e14c0372dbe12f233959a
TheOriginalDollarStoreBaller <stps87@gmail.com>
2017-07-26 19:14:10 -0700

    podcast.hbs file works

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+// MODAL FUNCTION FOR EMAIL SIGN UP BELOW
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+   
+    $('#myModal').modal({
+        keyboard: true,
+        show: false,
+    })
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('.blue-box').mouseover(function(){
+    console.log("you clicked me!")
+    $('.blue-box').addClass('grower').removeClass('shrink')
+})
+$('.blue-box').mouseout(function(){
+    console.log("you clicked me!")
+
+    $('.blue-box').addClass('shrink').removeClass('grower')
+})
+
+$('.podcast-card').mouseenter(function(){
+    console.log("you clicked me!")
+    $(this).children('a').children('.podcast-image-writing').children('.written-content').addClass('grower').removeClass('shrink')
+})
+$('.podcast-card').mouseleave(function(){
+    console.log("you clicked me!")
+
+    $(this).children('a').children('.podcast-image-writing').children('.written-content').addClass('shrink').removeClass('grower')
+})
+
+});
+
+// $('.comments').on('click','.hide-replies',function(){
+//     $(this).closest('.comment').find('.replies').hide();
+// })
