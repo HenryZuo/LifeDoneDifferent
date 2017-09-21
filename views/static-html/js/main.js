83c9a77270f51fb9630a6f83c716c4bc36e26c77
TheOriginalDollarStoreBaller <stps87@gmail.com>
2017-09-20 18:54:47 -0700

    added unique head info via script on onePodcast and moved jquery above body in main.hbs to understand script tag in onePodcast

--- views/static-html/js/main.js
+++ views/static-html/js/main.js
@@ -1,37 +1,39 @@
 // MODAL FUNCTION FOR EMAIL SIGN UP BELOW
 
-$( document ).ready(function() {
-    console.log( "ready!" );
-   
+$(document).ready(function () {
+    console.log("ready!");
+
     $('#myModal').modal({
         keyboard: true,
         show: false,
     })
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-})
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').focus()
+    })
 
-$('.blue-box').mouseover(function(){
-    console.log("you clicked me!")
-    $('.blue-box').addClass('grower').removeClass('shrink')
-})
-$('.blue-box').mouseout(function(){
-    console.log("you clicked me!")
+    $('.blue-box').mouseover(function () {
+        console.log("you clicked me!")
+        $('.blue-box').addClass('grower').removeClass('shrink')
+    })
+    $('.blue-box').mouseout(function () {
+        console.log("you clicked me!")
 
-    $('.blue-box').addClass('shrink').removeClass('grower')
-})
+        $('.blue-box').addClass('shrink').removeClass('grower')
+    })
 
-$('.podcast-card').mouseenter(function(){
-    console.log("you clicked me!")
-    $(this).children('a').children('.podcast-image-writing').children('.written-content').addClass('grower').removeClass('shrink')
-})
-$('.podcast-card').mouseleave(function(){
-    console.log("you clicked me!")
+    $('.podcast-card').mouseenter(function () {
+        console.log("you clicked me!")
+        $(this).children('a').children('.podcast-image-writing').children('.written-content').addClass('grower').removeClass('shrink')
+    })
+    $('.podcast-card').mouseleave(function () {
+        console.log("you clicked me!")
 
-    $(this).children('a').children('.podcast-image-writing').children('.written-content').addClass('shrink').removeClass('grower')
-})
+        $(this).children('a').children('.podcast-image-writing').children('.written-content').addClass('shrink').removeClass('grower')
+    })
+    $('head').append('<meta property="og:locale:THIS IS A TEST" content="fr_FR" />')
 
 });
+
 
 // $('.comments').on('click','.hide-replies',function(){
 //     $(this).closest('.comment').find('.replies').hide();
